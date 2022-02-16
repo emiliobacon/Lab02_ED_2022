75df0783ac4eb67e795001871cf394fd78c052ff
EMILIO ANTONIO BARILLAS CONTRERAS <eabarillasco@correo.url.edu.gt>
2022-02-16 11:55:46 -0600

    updated generic list methods

--- Lab02/GenericList.cs
+++ Lab02/GenericList.cs
@@ -23,5 +23,73 @@ namespace Lab02
             
         }
 
+        void InsertNew (T value, int index)
+        {
+            if (index == 0)
+            {
+                InsertNew(value);
+            }
+            else
+            {
+                Node<T> NewNode = new Node<T>();
+                NewNode.data = value;
+                int CurrentPos = 0;
+                Node<T> CurrentNode = Head;
+                while (CurrentPos < index)
+                {
+                    CurrentNode = CurrentNode.next;
+                    CurrentPos++;
+                }
+                NewNode.next = CurrentNode;
+                CurrentNode.previous.next = NewNode;
+                NewNode.previous = CurrentNode.previous;
+                if (NewNode.next != null)
+                {
+                    CurrentNode.next.previous = NewNode;
+                }
+            }
+        }
+
+        int Search (T value)
+        {
+            int count = 0;
+            for (Node<T> index = Head;index != null; index = index.next)
+            {
+                if (EqualityComparer<T>.Default.Equals(index.data, value))
+                {
+                    return count;
+                }
+                count++;
+            }
+            return -1;
+        }
+
+        bool Delete(T value)
+        {
+            Node<T> Current = Head;
+            int index = Search(value);
+            if (index == -1)
+            {
+                return false;
+            }
+            else if (index == 0)
+            {
+                Current = Head;
+                Head.next.previous = null;
+                Head = Head.next;
+            }
+            else
+            {
+                for (int i = 0; i < index; i++)
+                {
+                    Current = Current.next;
+                }
+                Current.previous.next = Current.next;
+                Current.next.previous = Current.previous;
+            }
+            //Hay que eliminar el nodo current
+            return true;
+        }
+
     }
 }
