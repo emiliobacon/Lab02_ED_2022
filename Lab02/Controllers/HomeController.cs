71436bd961b08b759442e4ba93549f02633d5739
EMILIO ANTONIO BARILLAS CONTRERAS <eabarillasco@correo.url.edu.gt>
2022-02-17 03:49:33 -0600

    csv reader

--- Lab02/Controllers/HomeController.cs
+++ Lab02/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Lab02.Controllers
 {
@@ -34,6 +35,9 @@ namespace Lab02.Controllers
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
 
-       
+        
+        
+
+
     }
 }
--- Lab02/Controllers/TeamController.cs
+++ Lab02/Controllers/TeamController.cs
@@ -1,5 +1,11 @@
 ﻿using Lab02.Helpers;
 using Lab02.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -91,6 +97,70 @@ namespace Lab02.Controllers
             {
                 return View();
             }
+        }
+
+        //leer csv 
+
+        private IHostingEnvironment Environment;
+        public TeamController(IHostingEnvironment _environment)
+        {
+            Environment = _environment;
+        }
+        
+
+        [HttpPost]
+        public IActionResult Index(IFormFile postedFile)
+        {
+            if (postedFile != null)
+            {
+                string path = Path.Combine(this.Environment.WebRootPath, "Uploads");
+                if (!Directory.Exists(path))
+                {
+                    Directory.CreateDirectory(path);
+                }
+
+                string fileName = Path.GetFileName(postedFile.FileName);
+                string filePath = Path.Combine(path, fileName);
+                using (FileStream stream = new FileStream(filePath, FileMode.Create))
+                {
+                    postedFile.CopyTo(stream);
+                }
+                string csvData = System.IO.File.ReadAllText(filePath);
+                DataTable dt = new DataTable();
+                bool firstRow = true;
+                foreach (string row in csvData.Split('\n'))
+                {
+                    if (!string.IsNullOrEmpty(row))
+                    {
+                        if (!string.IsNullOrEmpty(row))
+                        {
+                            if (firstRow)
+                            {
+                                foreach (string cell in row.Split(','))
+                                {
+                                    
+                                    dt.Columns.Add(cell.Trim());
+                                }
+                                firstRow = false;
+                            }
+                            else
+                            {
+                                dt.Rows.Add();
+                                int i = 0;
+                                foreach (string cell in row.Split(','))
+                                {
+                                    dt.Rows[dt.Rows.Count - 1][i] = cell.Trim();
+                                    i++;
+                                }
+                            }
+                        }
+                    }
+                }
+
+                return View(dt);
+            }
+
+            return View();
         }
     }
 }
