67f0475c0d85731cb35f79952a64564336f09868
Megan-Morales <meganm.beta@gmail.com>
2022-02-23 17:54:57 -0600

    Agregar búsqueda y filtrado de jugadores

---
+++ Lab02/Helpers/Search.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Lab02.Models;
+
+namespace Lab02.Helpers
+{
+    public class Search
+    {
+        void searchTeam(List<TeamModel> list)
+        {
+
+        }
+    }
+}
--- Lab02/Models/PlayerModel.cs
+++ Lab02/Models/PlayerModel.cs
@@ -1,6 +1,7 @@
 ﻿using Lab02.Helpers;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,11 +9,17 @@ namespace Lab02.Models
 {
     public class PlayerModel
     {
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string LastName { get; set; }
+        [Required]
         public string Rol { get; set; }
+        [Required]
         public float KDA { get; set; }
+        [Required]
         public int CreepScore { get; set; }
+        [Required]
         public string Team { get; set; }
 
         public static void Save(PlayerModel model)
